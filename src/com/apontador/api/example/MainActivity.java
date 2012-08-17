b90ac2dd7a257d9843f38f5aa2adc13489ab7191
Wanderlei Souza <wanderlei.souza@apontador.com>
2012-08-17 17:40:17 -0300

    buttons in main_layout.xml

--- src/com/apontador/api/example/MainActivity.java
+++ src/com/apontador/api/example/MainActivity.java
@@ -1,20 +1,42 @@
 package com.apontador.api.example;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        View passwordButton = findViewById(R.id.button_passwordflow);
+        passwordButton.setOnClickListener(this);
+        
+        View clientCredentialsButton = findViewById(R.id.button_clientcredentialsflow);
+        clientCredentialsButton.setOnClickListener(this);
+        
     }
+    
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.button_passwordflow:
+			
+			break;
+		case R.id.button_clientcredentialsflow:
+			
+			break;			
+		}
+	}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+
 }
